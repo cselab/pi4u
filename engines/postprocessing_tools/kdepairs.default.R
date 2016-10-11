ad5ed6082fdb397222decaa13063c92c50a1f996
Lina Kulakova <lina.kulakova@mavt.ethz.ch>
2016-10-11 16:44:39 +0200

    Code style + simplifications for postprocessing tools

--- engines/postprocessing_tools/kdepairs.default.R
+++ engines/postprocessing_tools/kdepairs.default.R
@@ -1,12 +1,7 @@
-kdepairs.default <- function(x, n_1d = 20, n_2d = 25, labels, density = TRUE,
-                             contour = FALSE, ...) {
+kdepairs.default <- function(x, n_1d=20, n_2d=25, labels, density=TRUE,
+                             contour=FALSE, ...) {
     require(MASS)
     require(sp)
-
-    ly <- length(x)-1
-    last <- length(x)
-    y <- data.frame(x[, 1:ly])
-    assign("loglh", data.frame(x[, last]), envir = .GlobalEnv)
 
     fun.lower <- function(x1, x2, ...) {
         if (is.factor(x1)) x1 <- as.integer(x1)
@@ -17,12 +12,12 @@ kdepairs.default <- function(x, n_1d = 20, n_2d = 25, labels, density = TRUE,
 
         if (n_2d > 0 && OK) {
             if (density || contour) {
-                d <- tryCatch(kde2d(x1, x2, n = n_2d), error = function(e) {
+                d <- tryCatch(kde2d(x1, x2, n=n_2d), error=function(e) {
                     if (e$message == "bandwidths must be strictly positive") {
                         cat("Warning: resetting the bandwidths")
                         hx <- 0.1*(max(x1)-min(x1))
                         hy <- 0.1*(max(x2)-min(x2))
-                        d <- kde2d(x1, x2, n = n_2d, h = c(hx, hy))
+                        d <- kde2d(x1, x2, n=n_2d, h=c(hx, hy))
                     } else cat("Error caught! Handling not implemented!")
                 })
             }
@@ -30,12 +25,12 @@ kdepairs.default <- function(x, n_1d = 20, n_2d = 25, labels, density = TRUE,
             if (density) {
                 # library("colorspace"); vv<-c("white", heat_hcl(12))
                 # library(RColorBrewer); vv<-c("white", rev(brewer.pal(9, "YlOrRd")))
-                # vv<-c("white", rev(bpy.colors(n = 10, cutoff.tails = 0.3, alpha = 0.7)))
-                vv<-c("white", bpy.colors(n = 100, cutoff.tails = 0.3, alpha = 0.7))
-                image(d, col = vv, add = TRUE)
+                # vv<-c("white", rev(bpy.colors(n=10, cutoff.tails=0.3, alpha=0.7)))
+                vv<-c("white", bpy.colors(n=100, cutoff.tails=0.3, alpha=0.7))
+                image(d, col=vv, add=TRUE)
             }
 
-            if (contour) graphics:::contour(d, add = TRUE, nlevels = 8)
+            if (contour) graphics:::contour(d, add=TRUE, nlevels=8)
         } else points(x1, x2)
     }
 
@@ -43,22 +38,23 @@ kdepairs.default <- function(x, n_1d = 20, n_2d = 25, labels, density = TRUE,
         if (is.factor(x1)) x1 <- as.integer(x1)
         if (is.factor(x2)) x1 <- as.integer(x2)
 
-        vv<-bpy.colors(n = 100, cutoff.tails = 0.3, alpha = 0.5)
+        vv<-bpy.colors(n=100, cutoff.tails=0.3, alpha=0.5)
 
-        # This adds a column of color values based on the y values
-        datc <- vv[as.numeric(cut(as.numeric(loglh[, 1]), breaks = 100))]
-        points(x1, x2, col = datc, pch = 16, cex = 0.5)
+        # This adds a column of color values based on the last column values
+        # datc <- vv[as.numeric(cut(as.numeric(loglh), breaks=100))]
+        datc <- vv[cut(loglh, breaks=100)]
+        points(x1, x2, col=datc, pch=16, cex=0.5)
     }
 
-    fun.diag.kde = function(x, ...) {
+    fun.diag.kde <- function(x, ...) {
         pu <- par("usr")
 
         d <- density(x)
 
-        par("usr" = c(pu[1:2], 0, max(d$y)*1.5))
-        polygon(d, col = "green", border = "black")
+        par("usr"=c(pu[1:2], 0, max(d$y)*1.5))
+        polygon(d, col="green", border="black")
 
-        par("usr" = c(pu[1:2], 0, 1))
+        par("usr"=c(pu[1:2], 0, 1))
         # the following 2 lines draw a box-and-whiskers plot
         # the bottom and top of the box are always the first and third quartiles,
         # and the band inside the box is always the second quartile (the median)
@@ -66,28 +62,28 @@ kdepairs.default <- function(x, n_1d = 20, n_2d = 25, labels, density = TRUE,
         # to the square-roots of the number of observations in the groups.
         # If range is positive, the whiskers extend to the most extreme data point
         # which is no more than range times the interquartile range from the box.
-        h <- boxplot(x, plot = FALSE, range = 1.5)
-        bxp(h, at = 0.1, varwidth = TRUE, boxwex = 0.1, horizontal = TRUE,
-            outline = FALSE, add = TRUE, axes = FALSE)
+        h <- boxplot(x, plot=FALSE, range=1.5)
+        bxp(h, at=0.1, varwidth=TRUE, boxwex=0.1, horizontal=TRUE,
+            outline=FALSE, add=TRUE, axes=FALSE)
     }
 
     fun.diag.hist <- function(x, ...) {
         pu <- par("usr") # don't delete pu -- it's for correct scaling!
 
-        # xm = quantile(x, 0.01)
-        # xM = quantile(x, 0.99)
+        # xm <- quantile(x, 0.01)
+        # xM <- quantile(x, 0.99)
         # x <- x[ x>xm ]
         # x <- x[ x<xM ]
 
         nB <- n_1d+1
-        br <- seq(min(x), max(x), length.out = nB)
-        h <- hist(x, breaks = br, plot = FALSE)
+        br <- seq(min(x), max(x), length.out=nB)
+        h <- hist(x, breaks=br, plot=FALSE)
         y <- h$density
 
-        par("usr" = c(pu[1:2], 0, max(y)*1.5))
-        rect(br[-nB], 0, br[-1], y, col = "green")
+        par("usr"=c(pu[1:2], 0, max(y)*1.5))
+        rect(br[-nB], 0, br[-1], y, col="green")
 
-        par("usr" = c(pu[1:2], 0, 1))
+        par("usr"=c(pu[1:2], 0, 1))
         # the following 2 lines draw a box-and-whiskers plot
         # the bottom and top of the box are always the first and third quartiles,
         # and the band inside the box is always the second quartile (the median)
@@ -95,12 +91,14 @@ kdepairs.default <- function(x, n_1d = 20, n_2d = 25, labels, density = TRUE,
         # to the square-roots of the number of observations in the groups.
         # If range is positive, the whiskers extend to the most extreme data point
         # which is no more than range times the interquartile range from the box.
-        h <- boxplot(x, plot = FALSE, range = 1.5)
-        bxp(h, at = 0.1, varwidth = TRUE, boxwex = 0.1, horizontal = TRUE,
-            outline = FALSE, add = TRUE, axes = FALSE)
+        h <- boxplot(x, plot=FALSE, range=1.5)
+        bxp(h, at=0.1, varwidth=TRUE, boxwex=0.1, horizontal=TRUE,
+            outline=FALSE, add=TRUE, axes=FALSE)
     }
 
-    pairs.default(y, labels = labels, lower.panel = fun.lower,
-                  upper.panel = fun.upper, diag.panel = fun.diag.hist)
+    nd <- dim(x)[2]
+    loglh <- x[, nd]
+    pairs.default(x[, 1:nd-1], labels=labels, lower.panel=fun.lower,
+                  upper.panel=fun.upper, diag.panel=fun.diag.hist)
     invisible(NULL)
 }
--- engines/postprocessing_tools/kdeplotter.R
+++ engines/postprocessing_tools/kdeplotter.R
@@ -2,48 +2,53 @@ source("kdepairs.default.R")
 source("kdepairs.R")
 par(cex.axis=2)
 
+library(tools)
+
 cat("Did you delete all the variables you wanted? If no, run 'rm(list=ls())'")
 
 # ----------------------------------------------------------------------
 # name of the data file
-name = "curgen_db_007"
+fname <- "curgen_db_007.txt"
 
 # names of variables
 l = c(expression(theta[1]), expression(theta[2]))
 
-# flag saying if we use discrepancy of likelihood; used only for colouring
-discrepancy <- 0
-
-# flag saying if we use likelihood/log-likelihood
-truelik <- 0
+discrepancy <- 0  # is the last column of the data discrepancy of likelihood?
+truelik <- 0  # use likelihood or log-likelihood for coloring
 
 # ----------------------------------------------------------------------
 
-cat("Reading file", paste0(name,".txt"),"\n")
-data <- read.table(paste0(name, ".txt"))
-d <- dim(data)
+cat("Reading file", fname,"\n")
+data <- read.table(fname)
+data <- array(data=unlist(data), dim=dim(data))
+nd <- dim(data)[2]  # dimension of the samples
 
-md <- colMeans(data[1:d[2]-1])
-m2d <- colMeans(data[1:d[2]-1]^2)
-sd <- sqrt(m2d-md^2)
-COVd <- 100 * abs(sd / md)
-cat("means:", md, "\n")
-cat("stds:", sd, "\n")
-cat("COVs (%):", COVd, "\n")
+# mean
+md   <- colMeans(data[, 1:nd-1])
+cat("means:\t", md, "\n", sep="\t")
 
-best_id <- which.max( data[,d[2]] )
-cat("params with max likelihood: \n")
-print( data[best_id,] )
+# standard deviation
+m2d  <- colMeans(data[, 1:nd-1]^2)
+sd   <- sqrt(m2d-md^2)
+cat("stds:\t", sd, "\n", sep="\t")
 
-if(discrepancy) data[,d[2]]=-data[,d[2]]
+# coefficient of variation
+COVd <- 100*abs(sd/md)
+cat("COVs (%):", COVd, "\n", sep="\t")
 
-if(truelik) data[,d[2]]=exp(data[,d[2]])
+# most probable parameters
+best_id <- which.max(data[, nd])
+cat("MPVs:\t", data[best_id, 1:nd-1], "\n", sep="\t")
 
+if(discrepancy) data[ ,nd] <-    -data[ ,nd]
+if(truelik)     data[ ,nd] <- exp(data[ ,nd])
+
+pname <- paste0(file_path_sans_ext(fname), ".png")
 #------- this option is for big pictures ---------
-# png(paste0(name, ".png"), width=1080, height=1080, units='px', res=150, pointsize=10)
+# png(pname, width=1080, height=1080, units='px', res=150, pointsize=10)
 # kdepairs(data, n_1d=20, n_2d=200, labels=l)
 #------- this option is for small pictures -------
-png(paste0(name, ".png"), width=380, height=380, units='px', res=100, pointsize=10)
+png(pname, width=380, height=380, units='px', res=100, pointsize=10)
 kdepairs(data, n_1d=15, n_2d=200, labels=l)
 #-------------------------------------------------
 dev.off()
