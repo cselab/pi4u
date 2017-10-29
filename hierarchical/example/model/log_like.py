d67cfa598d73b0df82d4c03cb72a344335641fb3
Georgios Arampatzis <garampat@ethz.ch>
2017-10-29 16:30:50 +0100

    correct the likelihood

--- hierarchical/example/model/log_like.py
+++ hierarchical/example/model/log_like.py
@@ -13,26 +13,23 @@ def log_like( sigma_data ):
 	model = np.loadtxt('./result.txt')
 	model = model[:,1]
 
-	N = len(data.shape)
-
+        N = data.shape[0]
 
 	if N == 1 :
 		if np.isnan((model[1])):
 			return -1e12
-		sigma_data2 *= np.power( model[1], 2);
 		dif = data[1] - model[1]
 		ssn = np.power(dif,2)/sigma_data2
 
 	else:
-		if np.isnan(sum(model[:,1])):
+		if np.isnan(sum(model[:])):
 			return -1e12
-		sigma_data2 *= np.power( model[:,1], 2);
-		dif = data[:,1] - model[:,1]
+		dif = data - model
 		dif = dif*dif
 		ssn = np.sum( dif/sigma_data2 );
 
 
-	fval =  - 0.5*N*np.log(2.*np.pi) - 0.5*np.sum(np.log(sigma_data2))  - 0.5*ssn
+	fval =  - 0.5*N*np.log(2.*np.pi) - 0.5*N*np.log(sigma_data2)  - 0.5*ssn
 
 	return fval
 
@@ -45,6 +42,7 @@ import numpy as np
 p = np.loadtxt( sys.argv[1] )
 
 N = int(float(sys.argv[2]))
+
 
 res = log_like( p[N-1] )
 
