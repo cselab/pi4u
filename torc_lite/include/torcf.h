4fd20f2ac7a6d744f1c2fb29c73cd02b2bfb73bf
yaser afshar <ya.afshar@gmail.com>
2018-01-03 15:07:36 -0500

    Resolving the different preprocessors discrepency, because some Fortran compilers mangle names differently New interfaces defined for Fortran function calls (torc_broadcastf, torc_initf, torc_createf, torc_taskf)

--- torc_lite/include/torcf.h
+++ torc_lite/include/torcf.h
@@ -1,8 +1,7 @@
 C
 C     Predefined datatypes
 C
-C
-C
+
       INCLUDE 'mpif.h'
 
 C
@@ -12,8 +11,8 @@ C
       INTEGER MODE_MS
       INTEGER MODE_SPMD
 
-      PARAMETER (MODE_MS   =0)
-      PARAMETER (MODE_SPMD =1)
+      PARAMETER (MODE_MS   = 0)
+      PARAMETER (MODE_SPMD = 1)
 
       INTEGER CALL_BY_COP
       INTEGER CALL_BY_REF
@@ -23,40 +22,39 @@ C
       INTEGER CALL_BY_COP2
       INTEGER CALL_BY_VAD
 
-      PARAMETER (CALL_BY_COP  =x'0001')
-      PARAMETER (CALL_BY_REF  =x'0002')
-      PARAMETER (CALL_BY_RES  =x'0003')
-      PARAMETER (CALL_BY_PTR  =x'0004')
-      PARAMETER (CALL_BY_VAL  =x'0001')
-      PARAMETER (CALL_BY_COP2 =x'0005')
-      PARAMETER (CALL_BY_VAD  =x'0006')
+      PARAMETER (CALL_BY_COP  = x'0001')
+      PARAMETER (CALL_BY_REF  = x'0002')
+      PARAMETER (CALL_BY_RES  = x'0003')
+      PARAMETER (CALL_BY_PTR  = x'0004')
+      PARAMETER (CALL_BY_VAL  = x'0001')
+      PARAMETER (CALL_BY_COP2 = x'0005')
+      PARAMETER (CALL_BY_VAD  = x'0006')
 
 C
 C     INTERFACE
 C
+
       INTEGER*4 torc_worker_id
       INTEGER*4 torc_num_workers
-
       INTEGER*4 torc_i_worker_id
       INTEGER*4 torc_i_num_workers
-
       INTEGER*4 torc_node_id
       INTEGER*4 torc_num_nodes
+      INTEGER*4 torc_sched_nextcpu
 
-      DOUBLE precision torc_gettime
+      DOUBLE PRECISION torc_gettime
 
-      EXTERNAL torc_init
-
+      EXTERNAL torc_finalize
       EXTERNAL torc_taskinit
-      EXTERNAL torc_create
       EXTERNAL torc_waitall
-
       EXTERNAL torc_register_task
-
       EXTERNAL torc_enable_stealing
       EXTERNAL torc_disable_stealing
+      EXTERNAL torc_sleep
 
-      EXTERNAL torc_broadcast
+      EXTERNAL torc_broadcastf
+      EXTERNAL torc_initf
+      EXTERNAL torc_createf
+      EXTERNAL torc_taskf
 
-      INTEGER*4 torc_sched_nextcpu
 
