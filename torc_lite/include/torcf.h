9b94f1bec2d064068544d25a28db364fa9612aba
yaser afshar <ya.afshar@gmail.com>
2018-01-03 15:05:43 -0500

    clean up

--- torc_lite/include/torcf.h
+++ torc_lite/include/torcf.h
@@ -1,62 +1,62 @@
 C
-C Predefined datatypes
+C     Predefined datatypes
 C
 C
 C
-	include 'mpif.h'
+      INCLUDE 'mpif.h'
 
 C
 C
 C
 
-	INTEGER MODE_MS
-	INTEGER MODE_SPMD
+      INTEGER MODE_MS
+      INTEGER MODE_SPMD
 
-	PARAMETER (MODE_MS	=0)
-	PARAMETER (MODE_SPMD	=1)
+      PARAMETER (MODE_MS   =0)
+      PARAMETER (MODE_SPMD =1)
 
-	INTEGER CALL_BY_COP
-	INTEGER CALL_BY_REF
-	INTEGER CALL_BY_RES
-	INTEGER CALL_BY_PTR
-	INTEGER CALL_BY_VAL
-	INTEGER CALL_BY_COP2
-	INTEGER CALL_BY_VAD
+      INTEGER CALL_BY_COP
+      INTEGER CALL_BY_REF
+      INTEGER CALL_BY_RES
+      INTEGER CALL_BY_PTR
+      INTEGER CALL_BY_VAL
+      INTEGER CALL_BY_COP2
+      INTEGER CALL_BY_VAD
 
-	PARAMETER (CALL_BY_COP	=x'0001')
-	PARAMETER (CALL_BY_REF	=x'0002')
-	PARAMETER (CALL_BY_RES	=x'0003')
-	PARAMETER (CALL_BY_PTR	=x'0004')
-	PARAMETER (CALL_BY_VAL	=x'0001')
-	PARAMETER (CALL_BY_COP2	=x'0005')
-	PARAMETER (CALL_BY_VAD	=x'0006')
+      PARAMETER (CALL_BY_COP  =x'0001')
+      PARAMETER (CALL_BY_REF  =x'0002')
+      PARAMETER (CALL_BY_RES  =x'0003')
+      PARAMETER (CALL_BY_PTR  =x'0004')
+      PARAMETER (CALL_BY_VAL  =x'0001')
+      PARAMETER (CALL_BY_COP2 =x'0005')
+      PARAMETER (CALL_BY_VAD  =x'0006')
 
 C
-C Interface
+C     INTERFACE
 C
-	integer*4 torc_worker_id
-	integer*4 torc_num_workers
+      INTEGER*4 torc_worker_id
+      INTEGER*4 torc_num_workers
 
-	integer*4 torc_i_worker_id
-	integer*4 torc_i_num_workers
+      INTEGER*4 torc_i_worker_id
+      INTEGER*4 torc_i_num_workers
 
-	integer*4 torc_node_id
-	integer*4 torc_num_nodes
+      INTEGER*4 torc_node_id
+      INTEGER*4 torc_num_nodes
 
-	double precision torc_gettime
+      DOUBLE precision torc_gettime
 
-	external torc_init
-	
-	external torc_taskinit
-	external torc_create
-	external torc_waitall
+      EXTERNAL torc_init
 
-	external torc_register_task
+      EXTERNAL torc_taskinit
+      EXTERNAL torc_create
+      EXTERNAL torc_waitall
 
-	external torc_enable_stealing
-	external torc_disable_stealing
+      EXTERNAL torc_register_task
 
-	external torc_broadcast
+      EXTERNAL torc_enable_stealing
+      EXTERNAL torc_disable_stealing
 
-	integer*4 torc_sched_nextcpu
+      EXTERNAL torc_broadcast
+
+      INTEGER*4 torc_sched_nextcpu
 
